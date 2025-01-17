0a78858d2b3a407f7b96f8c033ebd1d53b314149
xiaolisheng <1522546196@qq.com>
2025-01-17 15:27:27 +0800

    first commit

---
+++ app/layouts/sidebar.tsx
@@ -0,0 +1,108 @@
+import {
+  Form,
+  Link,
+  NavLink,
+  Outlet,
+  useNavigation,
+  useSubmit,
+} from "react-router";
+import { getContacts } from "../data";
+import type { Route } from "./+types/sidebar";
+import { useEffect, useState } from "react";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+}
+
+export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
+  const { contacts, q } = loaderData;
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  // the query now needs to be kept in state
+  const [query, setQuery] = useState(q || "");
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  // we still have a `useEffect` to synchronize the query
+  // to the component state on back/forward button clicks
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q]);
+
+  return (
+    <>
+      <div id="sidebar">
+        <h1>
+          <Link to="about">React Router Contacts</Link>
+        </h1>
+        <div>
+          <Form
+            id="search-form"
+            onChange={(event) => {
+              const isFirstSearch = q === null;
+              submit(event.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}
+            role="search"
+          >
+            <input
+              aria-label="Search contacts"
+              className={searching ? "loading" : ""}
+              defaultValue={q || ""}
+              id="q"
+              name="q"
+              onChange={(event) => setQuery(event.currentTarget.value)}
+              placeholder="Search"
+              type="search"
+              value={query}
+            />
+            <div aria-hidden hidden={!searching} id="search-spinner" />
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? <span>★</span> : null}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div
+        className={navigation.state === "loading" ? "loading" : ""}
+        id="detail"
+      >
+        <Outlet />
+      </div>
+    </>
+  );
+}
